bc6af25f5a28ba266378d1110b67639851a43b16
Carlos D'Santiago <carlosdavidsantiago4@gmail.com>
2023-04-20 14:16:00 -0400

    agregando fetch para trabajar con Rest api

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
+({
+	plugins: ['jsdom-quokka-plugin'],
+	jsdom: {
+		file: 'index.html',
+	},
+});
+
 const input = document.getElementById('inputLetter');
 const form = document.getElementById('form');
 
@@ -6,7 +13,18 @@ form.addEventListener('submit', (e) => {
 	dataValidation(input.value);
 });
 
-const getData = () => {};
+const getData = () => {
+	fetch('https://restcountries.com/v3.1/all')
+		.then((resolve) => resolve.json())
+		.then((countriesData) => {
+			countriesData.forEach((country, index) => {
+				console.log(country.slice(0, 3));
+			});
+		})
+		.catch((error) => {
+			console.log(error);
+		});
+};
 
 function dataValidation(letter) {
 	const regex = /^[A-Za-z]$/;
