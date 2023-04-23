069c2d38e146ec68d844511488cab09d7e737b98
Carlos D'Santiago <carlosdavidsantiago4@gmail.com>
2023-04-22 22:30:56 -0400

    agregando los datos obtenidos, faltan algunos como el lenguaje y la moneda del pais

--- main.js
+++ main.js
@@ -8,19 +8,35 @@
 const input = document.getElementById('inputLetter');
 const form = document.getElementById('form');
 
+let url = 'https://restcountries.com/v3.1/all?fields=name,capital,flags,languages,currencies';
+
 form.addEventListener('submit', (e) => {
 	e.preventDefault();
 	dataValidation(input.value);
 });
 
-const getData = () => {
-	fetch('https://restcountries.com/v3.1/all')
+const getData = (letter) => {
+	fetch(url)
 		.then((resolve) => resolve.json())
 		.then((countriesData) => {
-			countriesData.forEach((country, index) => {
-				console.log(country.name.common);
+			const countryData = countriesData
+				.filter((country) => country.name.common.startsWith(letter.toUpperCase()))
+				.sort(() => Math.random() - 0.5)
+				.slice(0, 3);
+			console.log(countryData);
+			countryData.forEach((country) => {
+				document.getElementById('result').innerHTML += `
+				<div class="country-card">
+				<img src="${country.flags.png}">
+				<h3 class="country-name">${country.name.common}</h3>
+				<p class="country-capital">${country.capital}</p>
+				<p class="country-currency">${country.currencies.name}</p>
+				<ul class="country-language">${country.languages}</ul>
+				</div>
+				`;
 			});
 		})
+
 		.catch((error) => {
 			console.log(error);
 		});
@@ -31,6 +47,8 @@ function dataValidation(letter) {
 	const validLetter = regex.test(letter);
 
 	if (validLetter && letter.length === 1) {
-		return getData();
+		return getData(letter);
 	}
 }
+
+//Los datos a solicitar son: name,capital,flags,language,currencies
